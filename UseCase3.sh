b499579a8a41ec59858ccc2fa75d721ff089b999
unknown <varadesharad@gmail.com>
2020-04-18 13:32:01 +0530

    Add[UseCase7].Three value from user

--- UseCase3.sh
+++ UseCase3.sh
@@ -1,18 +1,30 @@
 #!/bin/bash -x
 
 
+
 A=$(( $a +$b * $c ))
 
+
 B=$(( $a * $b + $c ))
+
 C=$(( $c + $b / $c ))
+
 D=$(( $a % $b + $c ))
 
+
 result[UseCase1]=$A;
+
 result[UseCase2]=$B;
+
 result[UseCase3]=$C;
+
 result[Usecase4]=$D;
 
+
 echo "Use case2" ${result[A]}
+
 echo "Use Case 3" ${result[B]}
+
 echo "Use case 4" ${result[C]}
+
 echo  "Use case 5"${result[D]}
